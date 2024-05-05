ee9413845c7e7ec0b27fc8a4254a1bfc440d9d98
Josef Bruseke <josefbruseke@gmail.com>
2024-05-05 15:57:31 -0300

    add change

--- team-pool/src/app/(clubs)/page.tsx
+++ team-pool/src/app/(clubs)/page.tsx
@@ -1,27 +1,11 @@
+import ClubCard from "../components/ClubCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="grid grid-cols-1 sm:grid-cols-2 grid-rows-4 gap-8 w-full max-w-4xl items-center justify-between font-mono text-sm lg:flex">
         {/* Conteúdo da célula 1 */}
-        <div className="bg-gray-200 p-4">
-          Conteúdo da célula 1
-        </div>
-        
-        {/* Conteúdo da célula 2 */}
-        <div className="bg-gray-200 p-4">
-          Conteúdo da célula 2
-        </div>
-        
-        {/* Conteúdo da célula 3 */}
-        <div className="bg-gray-200 p-4">
-          Conteúdo da célula 3
-        </div>
-        
-        {/* Conteúdo da célula 4 */}
-        <div className="bg-gray-200 p-4">
-          Conteúdo da célula 4
-        </div>
+        <ClubCard/>        
       </div>
     </main>
   );
