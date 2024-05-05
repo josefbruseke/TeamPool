fab5a60e0fc57b380b17f0e9d7637a9f6d6ba0f1
Josef Bruseke <josefbruseke@gmail.com>
2024-05-05 11:09:37 -0300

    add grid cards

--- team-pool/src/app/(clubs)/page.tsx
+++ team-pool/src/app/(clubs)/page.tsx
@@ -2,8 +2,26 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        {/* Conteúdo da célula 1 */}
+        <div className="bg-gray-200 p-4">
+          Conteúdo da célula 1
+        </div>
         
+        {/* Conteúdo da célula 2 */}
+        <div className="bg-gray-200 p-4">
+          Conteúdo da célula 2
+        </div>
+        
+        {/* Conteúdo da célula 3 */}
+        <div className="bg-gray-200 p-4">
+          Conteúdo da célula 3
+        </div>
+        
+        {/* Conteúdo da célula 4 */}
+        <div className="bg-gray-200 p-4">
+          Conteúdo da célula 4
+        </div>
       </div>
     </main>
   );
