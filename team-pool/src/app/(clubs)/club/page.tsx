70f1d67b59e3841898782a4d4131eb0c9803f485
Josef Bruseke <josefbruseke@gmail.com>
2024-05-05 21:04:26 -0300

    add changes

--- team-pool/src/app/(clubs)/club/page.tsx
+++ team-pool/src/app/(clubs)/club/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import Button from "@/app/components/Button";
-import EventCalender from "@/app/components/EventCalender";
-import { addDays } from "date-fns";
+import ReserveCard from "@/app/components/ReserveCard";
 import { useState } from "react";
 
  
@@ -14,18 +13,18 @@ export default function Page() {
 	<div className="h-screen flex flex-col items-center">
 		<div className='flex flex-col md:flex-row gap-4 md:gap-20 p-4 pl-4 md:pl-20 mb-10'>
 			<Button color={active === 'descricao' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('descricao')} className="hover:bg-[#1B1B1B] hover:text-white">Descrição</Button>
-			<Button color={active === 'horarios' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('horarios')} className="hover:bg-[#1B1B1B] hover:text-white">Horários</Button>
+			<Button color={active === 'horarios' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('horarios')} className="hover:bg-[#1B1B1B] hover:text-white">Reservas</Button>
 		</div>
 		{active === 'descricao' && (
 			<>
-			<div className="bg-blue-100 h-80 w-1/2 rounded-xl">
+			<div className="bg-[#1971B4] h-80 w-1/2 rounded-xl">
 				  {/* Conteúdo da descrição */}
 			  </div>
 			  <div className="flex m-10 gap-10 w-1/2 h-1/3">
-					<div className="bg-blue-100 w-full rounded-xl">
+					<div className="bg-[#1971B4] w-full rounded-xl">
 						  {/* Conteúdo do primeiro bloco */}
 					  </div>
-					  <div className="bg-blue-100 w-full rounded-xl">
+					  <div className="bg-[#1971B4] w-full rounded-xl">
 						  {/* Conteúdo do segundo bloco */}
 					  </div>
 				  </div>
@@ -35,10 +34,7 @@ export default function Page() {
 
 		{active === 'horarios' && (
 			<div className="h-80 w-1/2 rounded-xl">
-			<EventCalender events={[
-					{date: addDays(new Date(), 6), title: "Futebol"}
-				  	]}
-				  />
+				<ReserveCard/>
 			</div>
 		)}
 		</div>
