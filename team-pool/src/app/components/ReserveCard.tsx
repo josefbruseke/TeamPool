2ab84fe19124c9f6efdd2eb0c02bfc8d18f9f1a5
Josef Bruseke <josefbruseke@gmail.com>
2024-05-05 21:04:55 -0300

    add reserve card

---
+++ team-pool/src/app/components/ReserveCard.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+export default function ClubCard() {
+    return (
+        <div className="bg-[#1971B4] w-80 p-2 rounded-xl text-white">
+            <p>Status: Aberto</p>
+            <p>Data: 11/05/2024</p>
+            <p>Horário: 20:00-21:00</p>
+            <p>Passes máximos: 16</p>
+            <p>Passes disponíveis: 3</p>
+            <Button className="h-5 m-2">Reservar</Button>
+        </div>
+    )
+}
