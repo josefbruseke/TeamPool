4296ab116634aa480edd68efeea4ddbd24d889cb
Josef Bruseke <josefbruseke@gmail.com>
2024-08-08 11:56:00 -0300

    add pool page

---
+++ team-pool/src/app/pools/pool/page.tsx
@@ -0,0 +1,20 @@
+import Button from "@/app/components/Button";
+
+
+interface Pool {
+    creator: string,
+    date: Date,
+    location: string
+    price: boolean,
+    max_tokens: number
+    current_token: number
+}
+
+
+export default function Pool() {
+    return (
+        <div className="h-screen flex flex-col items-center bg-gray-100">
+
+        </div>
+    );
+}
